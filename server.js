1a52e831092586c74db8e96b13ece44dc55326e2
steven-tan-telus <steven.tan@telus.com>
2024-09-23 12:53:04 -0400

    added views, index.js and sycle.css

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const path = require("path");
 
-/* ***********************
- * Routes
- *************************/
-app.use(static)
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, "public")));
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout");
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
+app.use(require("./routes/static"));
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
+
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
